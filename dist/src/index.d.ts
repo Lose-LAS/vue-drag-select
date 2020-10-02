7924117858fe70f3d94a01b68ad46f21f0c1f883
Lose_LAS <nvidiattxsli@gmail.com>
2020-10-01 23:19:52 -0700

    gen typing for packAge

---
+++ dist/src/index.d.ts
@@ -0,0 +1,10 @@
+declare namespace _default {
+    export const version: string;
+    export { install };
+    export { vueDragSelect };
+    export { dragSelectOption };
+}
+export default _default;
+declare function install(Vue: any): void;
+import vueDragSelect from "../packages/vue-drag-select/index.js";
+import dragSelectOption from "../packages/drag-select-option/index.js";
---
+++ dist/src/utils/util.d.ts
@@ -0,0 +1,1 @@
+export function getValueByPath(object: any, prop: any): any;
